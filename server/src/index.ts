55c3521fd4d1a58d1228fbeac96ffd76076162d2
Reuben D'Souza <reubenjds@gmail.com>
2024-01-26 16:22:11 -0500

    feat: add dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { router } from "./routes/hotels";
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
