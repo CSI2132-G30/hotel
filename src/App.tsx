dd265ade9a02de801028ee5f3a3c04575b095611
jordanxlau <jordanxlau@yahoo.ca>
2024-01-26 13:50:54 -0500

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
 	return (
 		<h1 className='text-3xl font-bold underline pr-2 text-cyan-400'>
-			Hello world! <button className='btn btn-secondary'>Secondary</button>
+			<p>Hello world!</p>
+			<button className='btn btn-secondary'>Secondary</button>
 		</h1>
 	);
 }
