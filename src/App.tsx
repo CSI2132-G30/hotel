7b9ed56246dd37a5627a020fc947d6a1c8664df8
Reuben D'Souza <reubenjds@gmail.com>
2024-01-26 12:22:07 -0500

    chore: initialize react, tailwind and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<h1 className='text-3xl font-bold underline pr-2 text-cyan-400'>
+			Hello world! <button className='btn btn-secondary'>Secondary</button>
+		</h1>
+	);
+}
