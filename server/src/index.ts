f1feac49596cd8c0b7e5571d7a468467562628d6
Reuben D'Souza <reubenjds@gmail.com>
2024-03-04 18:14:14 -0500

    feat: add Hotel type

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,18 @@ import dotenv from "dotenv";
 
 import { router } from "./routes/hotels";
 
+declare global {
+	export type Hotel = {
+		id: number;
+		name: string;
+		chain: string;
+		stars: number;
+		numRooms: number;
+		address: string;
+		manager: number;
+	};
+}
+
 const app = express();
 
 dotenv.config();
@@ -11,4 +23,4 @@ dotenv.config();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/hotels", router);
-app.listen(4040);
+app.listen(4040, () => console.log("app listening on port 4040"));
