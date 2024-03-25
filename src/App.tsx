0f5a14cd434610a11b8d87b68977d1b13dbde65e
jordanxlau <jordanxlau@yahoo.ca>
2024-03-25 19:41:38 -0400

    feat: now the home page views the chains

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import HotelCard from "./HotelCard";
+//Based on code from: https://www.youtube.com/watch?v=qdCHEUaFhBk 
+import {useState, useEffect} from "react";
 
 export default function App() {
+	const [chains, setChains] = useState<any[]>([]) // fix from:https://stackoverflow.com/questions/44147937/property-does-not-exist-on-type-never
+
+	useEffect(() => {
+        fetch('http://localhost:4040/hotels/chains')
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				setChains(data);
+			})
+    }, []);
+
 	return (
 		<>
 			<div className='flex-column text-center'>
 				<h1 className='text-center text-6xl py-8'>
-					Many Hotels to Choose From!
+					Five World Class Chains
 				</h1>
 			</div>
 			<div className='flex flex-wrap items-center justify-center gap-5 relative'>
-				<HotelCard
-					img={"/toronto.jpg"}
-					name={"Toronto"}
-					desc={"The stunning city"}
-					link={"https://google.ca"}></HotelCard>
-				<HotelCard
-					img={"/montreal.jpg"}
-					name={"Montreal"}
-					desc={"The stunning city"}
-					link={"https://google.ca"}></HotelCard>
-				<HotelCard
-					img={"/ottawa.jpg"}
-					name={"Ottawa"}
-					desc={"The stunning city"}
-					link={"https://google.ca"}></HotelCard>
+
+				{chains && chains.map((chain) => (
+					<a key={chain.id} className='card hover:scale-105 transition-all duration-300'>
+						<div className='card w-96 h-96 shadow-xl'>
+							<figure>
+								<img src={"/"+chain.name+".jpg"} />
+							</figure>
+							<div className='card-body'>
+								<h2 className='card-title'>{chain.name}</h2>
+								<p>{chain.hq_address}</p>
+								<div className='card-actions justify-end'></div>
+							</div>
+						</div>
+					</a>
+				))}
 			</div>
 		</>
 	);
--- src/HotelCard.tsx
+++ src/HotelCard.tsx
@@ -1,23 +1,23 @@
-type HotelCard = {
-	name: string;
-	desc: string;
-	link: string;
-	img: string;
-};
+// type HotelCard = {
+// 	name: string;
+// 	desc: string;
+// 	link: string;
+// 	img: string;
+// };
 
-export default function HotelCard({ name, desc, link, img }: HotelCard) {
-	return (
-		<a className='card hover:scale-105 transition-all duration-300' href={link}>
-			<div className='card w-96 h-96 shadow-xl'>
-				<figure>
-					<img src={img} />
-				</figure>
-				<div className='card-body'>
-					<h2 className='card-title'>{name}</h2>
-					<p>{desc}</p>
-					<div className='card-actions justify-end'></div>
-				</div>
-			</div>
-		</a>
-	);
-}
+// export default function HotelCard({ name, desc, link, img }: HotelCard) {
+// 	return (
+// 		<a className='card hover:scale-105 transition-all duration-300' href={link}>
+// 			<div className='card w-96 h-96 shadow-xl'>
+// 				<figure>
+// 					<img src={img} />
+// 				</figure>
+// 				<div className='card-body'>
+// 					<h2 className='card-title'>{name}</h2>
+// 					<p>{desc}</p>
+// 					<div className='card-actions justify-end'></div>
+// 				</div>
+// 			</div>
+// 		</a>
+// 	);
+// }
