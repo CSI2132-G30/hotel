37b071336824bb953bcd1254c3263adca3f9e16a
Robert Zuchniak <robert.zuchniak@hotmail.com>
2024-03-27 15:30:27 -0400

    feat: add login component

--- src/Account.tsx
+++ src/Account.tsx
@@ -1,16 +1,54 @@
 export default function Account() {
-
-	return (
-		<>
-			<div className='flex-column text-center'>
-				<h1 className='text-center text-6xl py-8'>
-					Login
-				</h1>
-                <button className="btn btn-neutral">Login as Employee</button>
-
-                <button className="btn btn-neutral">Login as Customer</button>
-
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="w-screen h-[calc(57rem-268px)] flex items-center justify-center">
+        <div className="bg-slate-100 w-96 h-96 rounded-md border-2">
+          <form
+            method="POST"
+            className="h-20 w-fill flex items-center justify-top flex-col"
+          >
+            <div className="pt-6">
+              <span className="text-3xl text-black"> Login </span>
+            </div>
+            <div className="w-full pt4 pl-6">
+              <div className="justify-items-start">Username</div>
+            </div>
+            <div className="w-full pl-6 pr-6">
+              <input
+                type="text"
+                name="username"
+                className="input w-full bg-white"
+              />
+            </div>
+            <div className="w-full pt-6 pl-6">
+              <div className="justify-items-start">Password</div>
+            </div>
+            <div className="w-full pl-6 pr-6">
+              <input
+                type="password"
+                name="password"
+                className="input w-full bg-white"
+              />
+            </div>
+            <div className="pt-6 pl-6 pr-6 w-full items-center flex justify-items-center justify-center flex-wrap">
+              <button
+                type="submit"
+                name="register_user"
+                className="btn btn-s w-full rounded-3xl"
+              >
+                Login as User
+              </button>
+              <button
+                type="submit"
+                name="register_admin"
+                className="btn btn-s w-full rounded-3xl mt-4"
+              >
+                Login as Employee
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,24 +1,30 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-	return (
-		<div className='navbar bg-base-100'>
-			<div className='flex-1'>
-				<a className='btn btn-ghost text-xl'>Five Hotels Group</a>
-			</div>
-			<div className='flex-none'>
-				<ul className='menu menu-horizontal px-1'>
-					<li>
-						<a><Link className="nav-link font-weight-bold" to={`/`}>Home</Link></a>
-					</li>
-					<li>
-						<a><Link className="nav-link font-weight-bold" to={`/account`}>Account</Link></a>
-					</li>
-					<li>
-						<a><Link className="nav-link font-weight-bold" to={`/bookings`}>Book Now</Link></a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Five Hotels Group</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a className="nav-link font-weight-bold" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="nav-link font-weight-bold" href="/account">
+              Account
+            </a>
+          </li>
+          <li>
+            <a className="nav-link font-weight-bold" href="/bookings">
+              Book Now
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
