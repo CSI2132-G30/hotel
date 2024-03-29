74623aca74091eef48c6acdaa5c3665221a146c4
Robert Zuchniak <robert.zuchniak@hotmail.com>
2024-03-28 21:04:12 -0400

    feat: employee authentication middleware

--- server/src/middleware/auth.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-import 'dotenv/config';
-
--- server/src/routes/hotels.ts
+++ server/src/routes/hotels.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { pool } from "../database";
+import authenticate from "../middleware/auth_employee";
 
 const router = express.Router();
 
@@ -12,9 +13,10 @@ router.get("/", async (req, res) => {
 	res.json(hotels.rows);
 });
 
-router.get("/test", async (req, res) => {
+router.get("/test", authenticate, async (req, res) => {
 	const hotels = await pool.query<Hotel>('SELECT * FROM available_rooms_per_city');
 	res.json(hotels.rows);
+	console.log(res.locals.user);
   }
   );
 
