87f652f192418243fea9747879b10e7d1cbb6d7d
Reuben D'Souza <reubenjds@gmail.com>
2024-03-30 13:55:27 -0400

    feat: add props

--- src/HotelCard.tsx
+++ src/HotelCard.tsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react";
 import RoomCard from "./RoomCard";
+import axios from "axios";
 
-export default function () {
-  return (
-    <div className="justify-center flex gap-6">
-      <div className="card w-4/5 bg-primary text-primary-content">
-        <div className="card-body">
-          <h2 className="card-title">Hotel Name</h2>
-          <div className="card-actions">
-            <RoomCard />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export default function HotelCard({ h }: { h: Hotel }) {
+	const [rooms, setRooms] = useState<Room[]>([]);
+
+	async function getRooms() {
+		try {
+			const res = await axios.get(
+				`http://localhost:4040/hotels/rooms?hotel=${h.id}`
+			);
+			setRooms(res.data);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	}
+	useEffect(() => {
+		getRooms();
+	}, []);
+	return (
+		<div className='flex flex-row justify-center items-center w-full'>
+			<div className='collapse bg-base-200'>
+				<input type='checkbox' className='peer' />
+				<div className='collapse-title border border-base-300 bg-base-200 '>
+					{h.name}
+				</div>
+				<div className='collapse-content flex flex-col justify-center items-center gap-2'>
+					{rooms.map((r) => (
+						<RoomCard r={r}></RoomCard>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/RoomCard.tsx
+++ src/RoomCard.tsx
@@ -1,16 +1,18 @@
-export default function RoomCard() {
-  return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title text-slate-400">Room Number</h2>
-        <div className="card-actions">
-          <div className="badge badge-outline">Wi-Fi</div>
-          <div className="badge badge-outline">Internet</div>
-          <div>
-            <button className="btn btn-primary">Book Now</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+function RoomCard({ r }: { r: Room }): JSX.Element {
+	return (
+		<div className='card w-96 bg-base-100 shadow-xl'>
+			<div className='card-body'>
+				<h1 className='card-title'>Room Number: {r.number}</h1>
+				<h2>Price: {r.price}</h2>
+				<h3>Capacity: {r.capacity}</h3>
+				<p>{r.view}</p>
+				<p>Amenities: {r.amenities.join(", ")}</p>
+				<div className='card-actions justify-end'>
+					<button className='btn btn-primary'>Book Now</button>
+				</div>
+			</div>
+		</div>
+	);
 }
+
+export default RoomCard;
