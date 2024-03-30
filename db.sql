8f346baa4616cc11375c0bd38c012340222e9079
jordanxlau <jordanxlau@yahoo.ca>
2024-03-30 19:17:42 -0400

    feat: triggers for all constraints

--- db.sql
+++ db.sql
@@ -87,7 +87,7 @@ INSERT INTO hotel (
 	('Cozy Inn Roxboro', 5, 2.9, 'Calgary', 0, '3 Belgrade Cres',  '000 000 048'),
 	('Cozy Inn Westmount', 5, 4.8, 'Montreal', 0, '3 rue du Jardin',  '000 000 001'),
 	('Cozy Inn Pearson Airport', 5, 4.7, 'Toronto', 0, '234 Kiping Ave',  '000 000 004'),
-	('Little Victoria Inn', 5, 5.5, 'Victoria', 0, '135 Mattamy St',  '000 000 041');
+	('Little Victoria Inn', 5, 5.0, 'Victoria', 0, '135 Mattamy St',  '000 000 041');
 
 INSERT INTO room (
 	hotel ,
@@ -366,5 +366,5 @@ INSERT INTO booking (
 	(98, '222 222 222', '2024-01-01','2024-12-31',TRUE),
 	(145, '333 333 333', '2024-01-01','2024-04-30',TRUE),
 	(87, '444 444 444', '2024-03-23','2024-08-23',TRUE),
-	(150, '111 111 111', '2024-04-02','2024-01-01',FALSE),
+	(150, '111 111 111', '2024-04-02','2024-05-01',FALSE),
 	(121, '111 111 111', '2024-04-17','2024-04-19',FALSE);
--- server/src/index.ts
+++ server/src/index.ts
@@ -67,6 +67,7 @@ import { userRouter } from "./routes/users";
 	PRIMARY KEY (room_id, customer_id, start_date)
 )`);
 
+	// Addition of indices
 	await pool.query(`CREATE INDEX IF NOT EXISTS hotel_city ON hotel(city)`);
 
 	await pool.query(
@@ -90,6 +91,7 @@ import { userRouter } from "./routes/users";
 		`CREATE INDEX IF NOT EXISTS booking_end ON booking(end_date DESC)`
 	);
 
+	// This creates the view that aggregates 
 	await pool.query(`
 	CREATE OR REPLACE VIEW available_rooms_per_city AS
 	SELECT city, COUNT(*) AS num_available_rooms
@@ -109,47 +111,98 @@ import { userRouter } from "./routes/users";
 
 	// function for hotel count the trigger
 	await pool.query(`CREATE OR REPLACE FUNCTION update_hotel_count() RETURNS TRIGGER AS $$
-	BEGIN	
-	UPDATE chain SET num_hotels = chain.num_hotels+1 WHERE chain.id = NEW.chain;
+	BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		UPDATE chain SET num_hotels = chain.num_hotels+1 WHERE chain.id = NEW.chain;
+	END IF;
+	IF (TG_OP = 'DELETE') THEN
+		UPDATE chain SET num_hotels = chain.num_hotels-1 WHERE chain.id = NEW.chain;
+	END IF;
 	RETURN NEW;
 	END;
 	$$ LANGUAGE plpgsql;`);
 
 	//hotel count trigger
 	await pool.query(`CREATE OR REPLACE TRIGGER hotel_count
-    AFTER INSERT ON HOTEL
+    AFTER INSERT OR DELETE ON HOTEL
     FOR EACH ROW
     EXECUTE FUNCTION update_hotel_count();`);
 
 	// function for the room count trigger
 	await pool.query(`CREATE OR REPLACE FUNCTION update_room_count() RETURNS TRIGGER AS $$
 	BEGIN	
-	UPDATE hotel SET num_rooms = hotel.num_rooms+1 WHERE hotel.id = NEW.hotel;
+	IF (TG_OP = 'INSERT') THEN	
+		UPDATE hotel SET num_rooms = hotel.num_rooms+1 WHERE hotel.id = NEW.hotel;
+	END IF;
+	IF (TG_OP = 'DELETE') THEN
+		UPDATE hotel SET num_rooms = hotel.num_rooms-1 WHERE hotel.id = NEW.hotel;
+	END IF;
 	RETURN NEW;
 	END;
 	$$ LANGUAGE plpgsql;`);
 	
 	// room count trigger
 	await pool.query (`CREATE OR REPLACE TRIGGER room_count
-    AFTER INSERT ON ROOM
+    AFTER INSERT OR DELETE ON ROOM
     FOR EACH ROW
     EXECUTE FUNCTION update_room_count();`);
 
 	// function for the star verification trigger
-	await pool.query (`CREATE OR REPLACE FUNCTION verify_num_stars() RETURNS TRIGGER AS $$
-    BEGIN
+	await pool.query (`CREATE OR REPLACE FUNCTION verify_hotel() RETURNS TRIGGER AS $$
+	BEGIN
 		IF NEW.stars > 5 OR NEW.stars < 0 THEN
-			RAISE EXCEPTION 'Stars must be between 0 and 5. % has %', NEW.name, NEW.stars;
+			RAISE EXCEPTION 'Stars must be between 0 and 5. % has %.', NEW.name, NEW.stars;
+		END IF;
+		IF NEW.num_rooms < 0 THEN
+			RAISE EXCEPTION '% has % rooms.', NEW.name, NEW.num_rooms;
 		END IF;
 		RETURN NEW;
 	END;
     $$ LANGUAGE plpgsql;`);
 
 	// star verification trigger
-	await pool.query (`CREATE OR REPLACE TRIGGER verify_stars
-    BEFORE INSERT ON HOTEL
+	await pool.query (`CREATE OR REPLACE TRIGGER verify_hotel
+    BEFORE INSERT OR UPDATE ON HOTEL
     FOR EACH ROW
-    EXECUTE FUNCTION verify_num_stars();`);
+    EXECUTE FUNCTION verify_hotel();`);
+
+	// function for the booking verification trigger
+	await pool.query (`CREATE OR REPLACE FUNCTION verify_booking() RETURNS TRIGGER AS $$
+	BEGIN
+		IF NEW.start_date > NEW.end_date THEN
+			RAISE EXCEPTION 'A booking starts after it ends.';
+		END IF;
+		IF NEW.start_date BETWEEN (SELECT start_date FROM booking WHERE NEW.room_id = room_id) AND (SELECT end_date FROM booking WHERE NEW.room_id = room_id) THEN
+			RAISE EXCEPTION 'Room % is booked twice at the same time', NEW.room_id;
+		END IF;
+		IF NEW.end_date BETWEEN (SELECT start_date FROM booking WHERE NEW.room_id = room_id) AND (SELECT end_date FROM booking WHERE NEW.room_id = room_id) THEN
+			RAISE EXCEPTION 'Room % is booked twice at the same time', NEW.room_id;
+		END IF;
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;`);
+
+	// booking verification trigger
+	await pool.query (`CREATE OR REPLACE TRIGGER verify_booking
+	BEFORE INSERT OR UPDATE ON BOOKING
+	FOR EACH ROW
+	EXECUTE FUNCTION verify_booking();`);
+
+	// function for the room verification trigger
+	await pool.query (`CREATE OR REPLACE FUNCTION verify_room() RETURNS TRIGGER AS $$
+	BEGIN
+		IF NEW.capacity < 1 THEN
+			RAISE EXCEPTION 'Room % in hotel % has a capacity less than 1.', NEW.room_id, NEW.hotel;
+		END IF;
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;`);
+
+	// room verification trigger
+	await pool.query (`CREATE OR REPLACE TRIGGER verify_room
+	BEFORE INSERT OR UPDATE ON ROOM
+	FOR EACH ROW
+	EXECUTE FUNCTION verify_room();`);
 
 })();
 
